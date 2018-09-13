ecf958344b83c675037945d9fa9f8a74db76b8f7
Aleksander Gajewski <adiog@discovr.pl>
2018-09-13 12:37:41 +0200

    prerelease

--- source/network/Dispatcher.h
+++ source/network/Dispatcher.h
@@ -1,5 +1,5 @@
-// This file is a part of opengl-stickyman project.
-// Copyright 2018 Aleksander Gajewski <adiog@brainfuck.pl>.
+// This file is a part of treadmill-t2-opengl project.
+// Copyright 2018 Disco WTMH S.A.
 
 
 #pragma once
